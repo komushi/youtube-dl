03198ad35626216df15a55eb1df34310c5014c0c
Lei Xu <komushi@gmail.com>
2018-07-26 14:05:08 +0900

    dl-conver-ul-array bug fix

--- dl-convert-ul-array/youtube_dl_array.py
+++ dl-convert-ul-array/youtube_dl_array.py
@@ -17,7 +17,7 @@ class Logger(object):
 def run(url, job_id):
 	
 	if height_key in os.environ:
-		exec_cmd = 'mv {} tmp_mp4; ffmpeg -f mp4 -i tmp_mp4 -vf scale=800:' + os.environ[height_key] + ' {}; rm tmp_mp4; python s3-ul.py {} ' + job_id
+		exec_cmd = 'mv {} tmp_mp4; ffmpeg -f mp4 -i tmp_mp4 -vf scale=800:' + os.environ[height_key] + ' {}; rm tmp_mp4; python s3_ul_array.py {} ' + job_id
 	else:
 		exec_cmd = 'python s3_ul_array.py {} ' + job_id
 
